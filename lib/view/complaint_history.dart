599db947956a87751d62ef1cee34f4264e3c6672
prasanna89654 <poudellprasanna789@gmail.com>
2022-09-14 11:45:51 +0545

    nn

---
+++ lib/view/complaint_history.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class History extends StatefulWidget {
+  const History({Key? key}) : super(key: key);
+
+  @override
+  State<History> createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(),
+      ),
+    );
+  }
+}
